42af685e426b449abb17eb2071a5a801996ce932
redasalem <redasalem287@gmail.com>
2026-08-29 01:43:20 +0300

    feat: implement Clerk authentication with Navbar and Login page integration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from 'react'
+import { SignIn, SignUp, useUser } from '@clerk/react';
+import { Navigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({mode = "login"}) => {
+    const isRegister = mode === "register";
+    const { isLoaded , isSignedIn} = useUser();
+    if(isLoaded && isSignedIn){
+        return <Navigate to={'/dashboard'} replace/>
+    }
     return (
-        <div>Login</div>
-    );
+        <div className="min-h-screen w-full bg-[url('/login_bg.png')] text-slate-800 p-4 md:p-6 lg:p-8 flex items-center justify-center font-sans">
+            <div className='w-full flex justify-center py-2'>
+                {isRegister ? (
+                    <SignUp routing="path" path="/register" signInUrl="/login"
+                    fallbackRedirectUrl="/dashboard"/>
+                ) : (
+                    <SignIn routing="path" path="/login" signUpUrl="/register"
+                    fallbackRedirectUrl="/dashboard"/>
+                )}
+            </div>
+        </div>
+    )
 }
 
-export default Login;
+export default Login
