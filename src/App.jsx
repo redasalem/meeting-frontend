2d12086a244d58aa80fddeb0321ee116281466ef
Reda Salem <redasalem287@gmail.com>
2026-08-28 22:20:03 +0300

    feat: enhance routing with protected routes and additional pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { toaster } from 'react-hot-toast';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedLayout from './components/ProtectedLayout';
+import Sessions from './pages/Sessions';
 
 
 function App() {
@@ -10,7 +14,20 @@ function App() {
     <toaster/>
     <Routes>
       <Route path= "/login" element={<Login mode="login"/>} />
+
       <Route path= "/register" element={<Login mode="register"/>} />
+
+      {/* private Routes */}
+      <Route element={<ProtectedRoute/>}>
+      <Route element={<ProtectedLayout/>}>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/sessions' element={<Sessions/>}/>
+      <Route path='/pricing' element={<Pricing/>}/>
+      </Route>
+      <Route path='/meeting/:meetingId' element={<MeetingRoom/>}/>
+      </Route>
+      {/* other Routes */}
+      <Route path='*' element={<Navigate to={'dashboard'} replace />}/>
 
     </Routes>
 
